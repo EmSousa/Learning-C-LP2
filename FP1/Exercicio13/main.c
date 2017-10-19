3b6f9090e90d460b87053c74642d817fd8b738bb
EmSousa <emilio.duarte.4ever@gmail.com>
2017-10-19 17:52:42 +0100

    Pronto para entrega com todos os exercicios a funcionar

--- FP1/Exercicio13/main.c
+++ FP1/Exercicio13/main.c
@@ -5,63 +5,55 @@
  * 
  */
 int main(int argc, char** argv) {
-    float min1, min2, min3, min4, min5, seg1, seg2, seg3, seg4, seg5;
-    int segundoshora, segundos_totais = 0, hora, segundo, minuto;
-    
-    segundoshora = 3600;
-    
+    int segundos_totais = 0, segundos_insert, minutos_insert, hora, minuto;
+   
     puts("Duração do album de 5 músicas");
     puts("Insira a duração da primeira música");
     printf("Minutos: ");
-    scanf("%f", &min1);
+    scanf("%d", &minutos_insert);
     printf("Segundos: ");
-    scanf("%f", &seg1);
+    scanf("%d", &segundos_insert);
     
-    segundos_totais = min1 * 60;
-    segundos_totais += seg1;
+    segundos_totais += (minutos_insert * 60) + segundos_insert;
     
     puts("Insira a duração da segunda música");
     printf("Minutos: ");
-    scanf("%f", &min2);
+    scanf("%d", &minutos_insert);
     printf("Segundos: ");
-    scanf("%f", &seg2);
+    scanf("%d", &segundos_insert);
     
-    segundos_totais += min2 * 60;
-    segundos_totais += seg2;
+    segundos_totais += (minutos_insert * 60) + segundos_insert;
     
     puts("Insira a duração da terceira música");
     printf("Minutos: ");
-    scanf("%f", &min3);
+    scanf("%d", &minutos_insert);
     printf("Segundos: ");
-    scanf("%f", &seg3);
+    scanf("%d", &segundos_insert);
     
-    segundos_totais += min3 * 60;
-    segundos_totais += seg3;
+    segundos_totais += (minutos_insert * 60) + segundos_insert;
     
     puts("Insira a duração da quarta música");
     printf("Minutos: ");
-    scanf("%f", &min4);
+    scanf("%d", &minutos_insert);
     printf("Segundos: ");
-    scanf("%f", &seg4);
+    scanf("%d", &segundos_insert);
     
-    segundos_totais += min4 * 60;
-    segundos_totais += seg4;
+    segundos_totais += (minutos_insert * 60) + segundos_insert;
     
     puts("Insira a duração da quinta música");
     printf("Minutos: ");
-    scanf("%f", &min5);
+    scanf("%d", &minutos_insert);
     printf("Segundos: ");
-    scanf("%f", &seg5);
+    scanf("%d", &segundos_insert);
     
-    segundos_totais += min5 * 60;
-    segundos_totais += seg5;
+    segundos_totais += (minutos_insert * 60) + segundos_insert;
     
-    //Calculo da hora:min:seg
-        
-    hora = segundos_totais / segundoshora;
-    minuto = (segundos_totais - (segundoshora * hora)) / 60;
-    segundo = segundos_totais - (segundoshora * hora) - (minuto * 60);
+    //Calculo da hora:min:seg     
+    hora = segundos_totais / 3600;
+    segundos_totais = segundos_totais % 3600;
+    minuto = segundos_totais / 60;
+    segundos_totais = segundos_totais % 60;
     
-    printf("O albúm tem a duração de %dH : %dM : %dS", hora, minuto, segundo);
+    printf("O albúm tem a duração de %dH : %dM : %dS", hora, minuto, segundos_totais);
     return (0);
 }

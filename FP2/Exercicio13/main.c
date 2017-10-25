472fceb1dd230e5bf167604fbd11ff4f862bbdca
EmSousa <emilio.duarte.4ever@gmail.com>
2017-10-25 14:38:53 +0100

    Exercicio 13 feito

---
+++ FP2/Exercicio13/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    float temperatura_atual, diferenca_temperatura, tempo;
+    int temperatura_pretendida, tempo_segundos;
+    
+    puts("Tempo em segundos que demora o AC a atuar");
+    printf("Introduza a temperatura atual: ");
+    scanf("%f", &temperatura_atual);
+    
+    printf("Introduza a temperatura pretendida: ");
+    scanf("%d", &temperatura_pretendida);
+    
+    diferenca_temperatura = temperatura_atual - temperatura_pretendida;
+    
+    if(diferenca_temperatura < 0){
+        diferenca_temperatura = -(diferenca_temperatura);
+        tempo = diferenca_temperatura * 3;
+        tempo_segundos = tempo * 60;
+        printf("Serao necessario %d segundos para aquecer ate a temperatura desejada", tempo_segundos);
+                
+    }else if(diferenca_temperatura > 0){
+        tempo = diferenca_temperatura * 2;
+        tempo_segundos = tempo * 60;
+        printf("Serao necessario %d segundos para arrefecer ate a temperatura desejada", tempo_segundos);
+    
+    }else if(diferenca_temperatura == 0){
+        puts("Introduziu a mesma temperatura!!!");
+    }
+
+    return (EXIT_SUCCESS);
+}
+

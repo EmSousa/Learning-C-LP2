d754cf86d171455d272d5d92a98f5566f88ac324
EmSousa <emilio.duarte.4ever@gmail.com>
2017-10-25 15:38:14 +0100

    Alteração ao exercicio 14

--- FP2/Exercicio14/main.c
+++ FP2/Exercicio14/main.c
@@ -15,26 +15,17 @@ int main(int argc, char** argv) {
     printf("Introduza qual o montante a movimentar: ");
     scanf("%lf", &montante);
     
-    printf("Introduza o numero da operacao pretendida: \n1-Debito \n2-Credito \nOperacao: ");
-    scanf("%d", &op);
+    saldo_final = saldo_conta + montante;
     
-    switch (op){
-        //Débito
-        case 1:
-            saldo_final = saldo_conta - montante;
-            if(saldo_final < 0){
-                puts("ERRO! Operacao invalida por saldo insuficiente");
-            }else{
-                printf("Debito realizado com sucesso! \nSaldo atual = %.2f€", saldo_final);
-            }
-            break;
+    if(saldo_final < 0){
+        puts("ERRO! Operacao invalida por saldo insuficiente!");
         
-        //Crédito    
-        case 2:
-            saldo_final = saldo_conta + montante;
-            printf("Credito realizado com sucesso! \nSaldo atual = %.2f€", saldo_final);
-            break;
-    }
+    }else if(saldo_final < saldo_conta){
+        printf("Debito realizado com sucesso! \nSaldo atual: %.2lf€", saldo_final);
+        
+    }else{
+        printf("Credito realizado com sucesso! \nSaldo atual: %.2lf€", saldo_final);
+    } 
 
     return (EXIT_SUCCESS);
 }

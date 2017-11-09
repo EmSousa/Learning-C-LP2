3add1263fdf1fac002eb39f99d774eacdbc5c6c1
EmSousa <emilio.duarte.4ever@gmail.com>
2017-11-09 15:18:33 +0000

    Exercicio 7 modificado

--- FP4/Exercicio7/main.c
+++ FP4/Exercicio7/main.c
@@ -1,33 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: emili
- *
- * Created on 8 de Novembro de 2017, 22:02
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXIMO 200
-#define MINIMO 1
 /*
- * USAR O EXERCICIO 1
+ * 
  */
 
-int lerInteiro(){
+int lerInteiro(int minimo, int maximo){
     int numero;
     
     while (1){
         printf("Introduza o numero dentro do intervalo: ");
         scanf("%d", &numero);
-        if (numero <= MAXIMO && numero >= MINIMO){
-            break;
+        if (numero <= maximo && numero >= minimo){
+            return numero;
         } else {
             puts("Numero invalido");
         }
@@ -38,14 +23,11 @@ int lerInteiro(){
 int main(int argc, char** argv) {
     int numero;
     
-    puts("Saber se um numero esta dentro do intervao (1 - 200)");
+    puts("Saber se um numero esta dentro do intervao (1 - 20)");
     
-    numero = lerInteiro();
+    numero = lerInteiro(0, 20);
     
     printf("\nO %d esta dentro do intervalo!", numero);
     
     return (EXIT_SUCCESS);
 }
-
-
-

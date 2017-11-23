7cb18059a11ea097c8a7d2b6cc7c75f5799b8146
EmSousa <emilio.duarte.4ever@gmail.com>
2017-11-23 12:05:14 +0000

    Exercicio 9

--- FP6/Exercicio9/main.c
+++ FP6/Exercicio9/main.c
@@ -1,23 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: emili
- *
- * Created on 23 de Novembro de 2017, 11:11
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "utils.h"
+
+#define MAXIMO 45
 
 /*
- * 
+ * Ficha 6 - Exercicio 9
  */
+
 int main(int argc, char** argv) {
+    char string1[MAXIMO], string2[MAXIMO], string3[1] = ",";
+    
+    printf("Introduza uma frase: ");
+    lerString(string1, MAXIMO);
+    
+    printf("Introduza uma frase: ");
+    lerString(string2, MAXIMO);
+    
+    strcat(string1, string3);
+    
+    printf("A frase concatenada: %s", string1);
 
     return (EXIT_SUCCESS);
 }
